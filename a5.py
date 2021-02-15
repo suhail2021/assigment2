3526154526d2c950263c209ad2eebd92814ddfc6
suhail <78995086+suhail2021@users.noreply.github.com>
2021-02-15 08:57:52 +0530

    Add files via upload

---
+++ a5.py
@@ -0,0 +1,5 @@
+dic={1:"hai",2:7,"age":24}
+dic1={2:"oio",7:89,"python":"jhh"}
+print("/ndictionary with the use of in integar keys:")
+print("/ndictionary with the use of in mixed keys:")
+print(dic,dic1)
